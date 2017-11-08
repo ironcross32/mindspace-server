20e9bac8c7bd6f5794372936f2676ed13192f2c1
Chris Norman <chris.norman2@googlemail.com>
2017-11-08 07:44:30 +0000

    Added codes to entrances.

--- server/db/entrances.py
+++ server/db/entrances.py
@@ -2,11 +2,15 @@
 
 from sqlalchemy import Column, Boolean, String
 from attrs_sqlalchemy import attrs_sqlalchemy
-from .base import Base, CoordinatesMixin, AmbienceMixin, LocationMixin
+from .base import (
+    Base, CoordinatesMixin, AmbienceMixin, LocationMixin, PasswordMixin
+)
 
 
 @attrs_sqlalchemy
-class Entrance(Base, CoordinatesMixin, AmbienceMixin, LocationMixin):
+class Entrance(
+    Base, CoordinatesMixin, AmbienceMixin, LocationMixin, PasswordMixin
+):
     """An entrance to another room."""
 
     __tablename__ = 'entrances'
@@ -17,6 +21,19 @@ class Entrance(Base, CoordinatesMixin, AmbienceMixin, LocationMixin):
     arrive_msg = Column(
         String(100), nullable=False, default='%1N arrive%1s from %2n.'
     )
+    locked = Column(Boolean, nullable=False, default=False)
+    code_msg = Column(
+        String(100), nullable=False, default='%1N fiddle%1s with %2n.'
+    )
+    code_sound = Column(String(100), nullable=True)
+    unlock_msg = Column(
+        String(100), nullable=False, default='%1N unlock%1s %2n.'
+    )
+    unlock_sound = Column(String(100), nullable=True)
+    lock_msg = Column(
+        String(100), nullable=False, default='%1N lock%1s %2n.'
+    )
+    lock_sound = Column(String(100), nullable=True)
 
     def get_all_fields(self):
         fields = super().get_all_fields()
