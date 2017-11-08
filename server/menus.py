92d7041e9963be98c3bbf6d7ac1857f3f2102544
Chris Norman <chris.norman2@googlemail.com>
2017-11-08 08:13:22 +0000

    Removed duplicate variables.

--- server/menus.py
+++ server/menus.py
@@ -36,10 +36,11 @@ class Menu:
 
 
 def objects_menu(
-    player, objects=True, exits=True, command_name='interact_object', *args,
-    **kwargs
+    player, objects=True, exits=True, command_name='interact_object',
+    *command_args, **command_kwargs
 ):
-    """Present a list of objects and / or exits close to a player."""
+    """Present a list of objects and / or exits close to a player. Send a
+    single id with command_args and command_kwargs to command_name."""
     con = player.get_connection()
     if con is None:
         return
@@ -62,7 +63,7 @@ def objects_menu(
         player.message('There is nothing at your current coordinates.')
     elif c == 1:
         id = q.first().id
-        con.handle_command(command_name, id, *args, **kwargs)
+        con.handle_command(command_name, id, *command_args, **command_kwargs)
     else:
         interface_sound(con, nonempty_room)
         items = [Item('Objects', None)]
@@ -70,7 +71,8 @@ def objects_menu(
             items.append(
                 Item(
                     f'{obj.get_name(player.is_staff)} ({obj.get_type()})',
-                    command_name, args=[obj.id, *args], kwargs=kwargs
+                    command_name, args=[obj.id, *command_args],
+                    kwargs=command_kwargs
                 )
             )
         m = Menu('Objects Menu', items, escapable=True)
