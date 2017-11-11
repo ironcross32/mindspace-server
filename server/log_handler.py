a835b1ef6995537a27132386aef88dc4ce149066
Chris Norman <chris.norman2@googlemail.com>
2017-11-11 20:31:28 +0000

    Change who sends log messages.

--- server/log_handler.py
+++ server/log_handler.py
@@ -1,7 +1,24 @@
 """Provides the LogHandler class."""
 
 from logging import Handler
-from .db import Session, CommunicationChannel, Player
+from attr import attrs, attrib
+from .db import Session, CommunicationChannel
+
+
+@attrs
+class PretendObject:
+    """A pretend user."""
+    name = attrib()
+    id = attrib()
+
+    def get_name(self, staff):
+        if not staff:
+            return self.name
+        else:
+            return f'{self.name} (#{self.id})'
+
+
+obj = PretendObject('System', 0)
 
 
 class LogHandler(Handler):
@@ -10,18 +27,11 @@ class LogHandler(Handler):
     def emit(self, record):
         """Send the record in game."""
         name = record.levelname
-        obj = Session.query(Player).filter_by(admin=True).limit(1).first()
-        if obj is None:
-            return  # No admins yet.
-        obj = obj.object
-        channel = Session.query(
-            CommunicationChannel
-        ).filter_by(name=name, admin=True).first()
+        channel = CommunicationChannel.query(name=name, admin=True).first()
         if channel is None:
             channel = CommunicationChannel(
-                name=name,
                 description=f'Log messages from the {name} category.',
-                admin=True
+                name=name, admin=True
             )
             Session.add(channel)
             Session.commit()
