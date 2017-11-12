5e30994aee8e357b74d9a57c243341f85162aa7f
Chris Norman <chris.norman2@googlemail.com>
2017-11-12 21:25:55 +0000

    Add CopyItem.

--- server/menus.py
+++ server/menus.py
@@ -20,6 +20,11 @@ class Item:
         return [self.title, self.command, self.args, self.kwargs]
 
 
+class CopyItem(Item):
+    def __init__(self, text):
+        return super().__init__(text, 'copy', args=[text])
+
+
 @attrs
 class Menu:
     """A menu."""
