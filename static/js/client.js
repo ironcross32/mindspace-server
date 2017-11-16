3e091e2327ccc6000bd8bcedcd173fc911ca5bbf
Chris Norman <chris.norman2@googlemail.com>
2017-11-16 17:51:27 +0000

    Debug printing.

--- static/js/client.js
+++ static/js/client.js
@@ -28,7 +28,7 @@ document.onkeydown = (e) => {
         key = e.key.toUpperCase()
         send({name: "key", args: [key, modifiers]})
     } else {
-        write_special(current.type)
+        write_special(`Key: ${current.type}.`)
     }
 }
 
