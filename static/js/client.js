af7bca59045b755072f872c18809d19f515dee18
Chris Norman <chris.norman2@googlemail.com>
2017-11-16 16:50:25 +0000

    Fix onkeydown.

--- static/js/client.js
+++ static/js/client.js
@@ -12,9 +12,11 @@ let output = document.getElementById("output")
 document.onkeydown = (e) => {
     let current = document.activeElement
     if (
-        connected && current.type === undefined || current.type in [
-            "text", "password", "textarea"
-        ]
+        connected && current.type === undefined || !(
+            current.type in [
+                "text", "password", "textarea"
+            
+        )]
     ) {
         modifiers = []
         for (name of ["ctrl", "shift", "alt"]) {
