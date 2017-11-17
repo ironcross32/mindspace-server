79e49dba61bf9be1e7ea4475f5625547980da132
Chris Norman <chris.norman2@googlemail.com>
2017-11-17 11:24:46 +0000

    Debug printing.

--- static/js/client.js
+++ static/js/client.js
@@ -297,7 +297,7 @@ let mindspace_functions = {
                             step = 1.0
                             empty = 0.0
                         }
-                        console.log(`${name}: ${type} (${tep}).`)
+                        console.log(`${name}: ${type} (${step}).`)
                         e.step = step
                     } else {
                         if (hidden) {
