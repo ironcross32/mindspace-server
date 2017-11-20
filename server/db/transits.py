9e5ead642093aa377ea884c09dd719b21be26667
Chris Norman <chris.norman2@googlemail.com>
2017-11-20 17:45:45 +0000

    Reinstated coordinates on Transit routes.

--- server/db/transits.py
+++ server/db/transits.py
@@ -29,7 +29,7 @@ class TransitStop(Base, LocationMixin, CoordinatesMixin):
         return sum([self.before_departure, self.after_departure])
 
 
-class TransitRoute(Base, NameMixin, PauseMixin):
+class TransitRoute(Base, NameMixin, CoordinatesMixin, PauseMixin):
     """Holds 0 or more transit stops."""
 
     __tablename__ = 'transit_routes'
