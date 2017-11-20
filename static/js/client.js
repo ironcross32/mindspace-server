c5cb16b0944db812ea88ef9c4204465ea46c40f1
Chris Norman <chris.norman2@googlemail.com>
2017-11-20 20:20:03 +0000

    Return true from enter and space.

--- static/js/client.js
+++ static/js/client.js
@@ -469,8 +469,8 @@ let menu_keys = {
         menu_index = Math.max(0, menu_index - 1)
         menu_ul.children[menu_index].firstChild.focus()
     },
-    "ENTER": () => write_message("Enter key."),
-    " ": () => false
+    "ENTER": () => true,
+    " ": () => true
 }
 
 function search_menu(e) {
