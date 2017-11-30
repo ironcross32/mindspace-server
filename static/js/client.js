7af9f709146f0f50473503a7b55b0bbe22308614
Chris Norman <chris.norman2@googlemail.com>
2017-11-30 15:00:30 +0000

    Remove debug printing.

--- static/js/client.js
+++ static/js/client.js
@@ -430,7 +430,6 @@ document.onkeydown = (e) => {
     ) {
         return
     }
-    write_special(e.key)
     let key = e.key.toUpperCase()
     if (escape === menu) {
         let func = menu_keys[key]
