0eef76fbaff93c0a3e984d7663c4986d2f3f6810
Chris Norman <chris.norman2@googlemail.com>
2017-12-03 20:58:34 +0000

    Add gender-specific social suffixes.

--- server/socials.py
+++ server/socials.py
@@ -39,3 +39,34 @@ def are(obj, suffix):
 def your(obj, suffix):
     """"your" or "name's"."""
     return ('your', f"{obj.get_name()}'s")
+
+
+@factory.suffix('he', 'she', 'it', 'subjective')
+def subjective(obj, suffix):
+    """"you" or the object's subjective pronoun."""
+    return ('you', obj.get_gender().subjective)
+
+
+@factory.suffix('him', 'her', 'o', 'objective')
+def objective(obj, suffix):
+    """"you" or the object's objective pronoun."""
+    return ('you', obj.get_gender().objective)
+
+
+@factory.suffix('his', 'its', 'p', 'pa')
+def possessive_adjective(obj, suffix):
+    """"your" or the object's possessive adjective."""
+    return ('your', obj.get_gender().possessive_adjective)
+
+
+@factory.suffix('hers', 'pn')
+def possessive_noun(obj, suffix):
+    """"your" or the object's possessive noun. Exactly the same as the your and
+    ss suffixes."""
+    return your(obj, suffix)
+
+
+@factory.suffix('himself', 'herself', 'itself', 'r', 'reflexive')
+def reflexive(obj, suffix):
+    """"yourself" or the object's reflexive pronoun."""
+    return ('yourself', obj.get_gender().reflexive)
