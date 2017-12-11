e41402a7129b8b24f1f902d78ae2e345f2d35a19
Chris Norman <chris.norman2@googlemail.com>
2017-12-11 20:05:59 +0000

    Updated ideas code.

--- server/db/ideas.py
+++ server/db/ideas.py
@@ -9,16 +9,8 @@ class IdeaVote(Base):
     """A vote for an idea."""
 
     __tablename__ = 'idea_votes'
-    idea_id = Column(
-        Integer,
-        ForeignKey('ideas.id'),
-        nullable=False
-    )
-    voter_id = Column(
-        Integer,
-        ForeignKey('objects.id'),
-        nullable=False
-    )
+    idea_id = Column(Integer, ForeignKey('ideas.id'), nullable=False)
+    voter_id = Column(Integer, ForeignKey('objects.id'), nullable=False)
 
 
 class Idea(Base, OwnerMixin):
@@ -28,23 +20,19 @@ class Idea(Base, OwnerMixin):
     title = Column(String(50), nullable=False, unique=True)
     body = Column(String(10000), nullable=False)
     created = Column(
-        DateTime(timezone=True),
-        nullable=False,
-        default=func.now()
+        DateTime(timezone=True), nullable=False, default=func.now()
     )
     votes = relationship(
-        'Object',
-        secondary=IdeaVote.__table__,
-        backref='idea_votes'
+        'Object', secondary=IdeaVote.__table__, backref='idea_votes'
     )
 
-    def __str__(self):
-        return '{0.title} (#{0.id})'.format(self)
-
     def add_comment(self, obj, text, parent=None):
-        """Add a comment containing text as obj. If parent is not None then the
-        new comment is considered in reply to parent."""
-        return IdeaComment(text=text, idea=self, owner=obj, parent=parent)
+        """Add a comment containing text with owner obj. If parent is not None
+        then the new comment is considered in reply to parent."""
+        return IdeaComment(
+            text=text, idea_id=self.id, owner_id=obj.id,
+            parent_id=None if parent is None else parent.id
+        )
 
 
 class IdeaComment(Base, OwnerMixin):
@@ -52,24 +40,14 @@ class IdeaComment(Base, OwnerMixin):
 
     __tablename__ = 'idea_comments'
     text = Column(String(10000), nullable=False)
-    idea_id = Column(
-        Integer,
-        ForeignKey('ideas.id'),
-        nullable=False
-    )
+    idea_id = Column(Integer, ForeignKey('ideas.id'), nullable=False)
     idea = relationship('Idea', backref='comments')
     parent_id = Column(
-        Integer,
-        ForeignKey(__tablename__ + '.id'),
-        nullable=True
+        Integer, ForeignKey(__tablename__ + '.id'), nullable=True
     )
     parent = relationship(
-        'IdeaComment',
-        backref='replies',
-        remote_side='IdeaComment.id'
+        'IdeaComment', backref='replies', remote_side='IdeaComment.id'
     )
     created = Column(
-        DateTime(timezone=True),
-        nullable=False,
-        default=func.now()
+        DateTime(timezone=True), nullable=False, default=func.now()
     )
