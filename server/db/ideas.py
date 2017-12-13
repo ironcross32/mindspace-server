ed5cbf423afd2a52e482c23e1378a330fd2a5179
Chris Norman <chris.norman2@googlemail.com>
2017-12-13 11:48:33 +0000

    Remove Idea.parent*.

--- server/db/ideas.py
+++ server/db/ideas.py
@@ -25,14 +25,6 @@ class Idea(Base, NameMixin, OwnerMixin):
         'Object', secondary=IdeaVote.__table__, backref='idea_votes'
     )
 
-    def add_comment(self, obj, text, parent=None):
-        """Add a comment containing text with owner obj. If parent is not None
-        then the new comment is considered in reply to parent."""
-        return IdeaComment(
-            text=text, idea_id=self.id, owner_id=obj.id,
-            parent_id=None if parent is None else parent.id
-        )
-
 
 class IdeaComment(Base, OwnerMixin):
     """A comment on an idea."""
@@ -41,12 +33,6 @@ class IdeaComment(Base, OwnerMixin):
     text = Column(String(10000), nullable=False)
     idea_id = Column(Integer, ForeignKey('ideas.id'), nullable=False)
     idea = relationship('Idea', backref='comments')
-    parent_id = Column(
-        Integer, ForeignKey(__tablename__ + '.id'), nullable=True
-    )
-    parent = relationship(
-        'IdeaComment', backref='replies', remote_side='IdeaComment.id'
-    )
     created = Column(
         DateTime(timezone=True), nullable=False, default=func.now()
     )
