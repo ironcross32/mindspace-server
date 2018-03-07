be847c1e52b4d97576d840ae843cea1eed205eac
Chris Norman <chris.norman2@googlemail.com>
2018-03-07 11:12:10 +0000

    Give Advert a created and last_shown column.

--- server/db/adverts.py
+++ server/db/adverts.py
@@ -1,10 +1,12 @@
 """Provides the Advert class."""
 
-from .base import Base, OwnerMixin, TextMixin, message
+from sqlalchemy import Column, DateTime
+from .base import Base, OwnerMixin, TextMixin, message, CreatedMixin
 
 
-class Advert(Base, OwnerMixin, TextMixin):
+class Advert(Base, OwnerMixin, TextMixin, CreatedMixin):
     """An advertisement."""
 
     __tablename__ = 'adverts'
     url = message(None, nullable=True)
+    last_shown = Column(DateTime(timezone=True), nullable=True)
