b0d6fcf5970d20b4b99aec37c685fcc8c158a463
Chris Norman <chris.norman2@googlemail.com>
2018-03-12 18:27:55 +0000

    Add permissions to commands.

--- server/db/commands.py
+++ server/db/commands.py
@@ -1,9 +1,9 @@
 """Provides the Command class."""
 
-from .base import Base, NameMixin, CodeMixin
+from .base import Base, NameMixin, CodeMixin, PermissionsMixin
 
 
-class Command(Base, NameMixin, CodeMixin):
+class Command(Base, NameMixin, CodeMixin, PermissionsMixin):
     """Respond to network connections."""
 
     __tablename__ = 'commands'
