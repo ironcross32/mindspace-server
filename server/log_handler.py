4d4bf056fabb6c17f578a6cce876d0b319fd86c0
Chris Norman <chris.norman2@googlemail.com>
2018-04-26 21:40:17 +0100

    Transmit log messages as the system object.

--- server/log_handler.py
+++ server/log_handler.py
@@ -1,24 +1,7 @@
 """Provides the LogHandler class."""
 
 from logging import Handler
-from attr import attrs, attrib
-from .db import Session, CommunicationChannel
-
-
-@attrs
-class PretendObject:
-    """A pretend user."""
-    name = attrib()
-    id = attrib()
-
-    def get_name(self, staff):
-        if not staff:
-            return self.name
-        else:
-            return f'{self.name} (#{self.id})'
-
-
-obj = PretendObject('System', 0)
+from .db import session, CommunicationChannel, ServerOptions
 
 
 class LogHandler(Handler):
@@ -27,15 +10,17 @@ class LogHandler(Handler):
     def emit(self, record):
         """Send the record in game."""
         name = record.levelname
-        channel = CommunicationChannel.query(name=name, admin=True).first()
-        if channel is None:
-            channel = CommunicationChannel(
-                description=f'Log messages from the {name} category.',
-                name=name, admin=True
+        with session() as s:
+            channel = CommunicationChannel.query(name=name, admin=True).first()
+            if channel is None:
+                channel = CommunicationChannel(
+                    description=f'Log messages from the {name} category.',
+                    name=name, admin=True
+                )
+                s.add(channel)
+                s.commit()
+            sender = ServerOptions.get().transmit_as
+            assert sender is not None
+            channel.transmit(
+                sender, f'{record.name}: {record.getMessage()}', strict=False
             )
-            Session.add(channel)
-            Session.commit()
-        channel.transmit(
-            obj, f'{record.name}: {record.getMessage()}', strict=False,
-            store=False
-        )
--- tests/db_test.py
+++ tests/db_test.py
@@ -1,5 +1,11 @@
-from server.db import Object
+from server.db import Object, ServerOptions
 
 
 def test_system():
     assert Object.get(0) is not None
+
+
+def test_transmit_as():
+    o = ServerOptions.get()
+    assert o.transmit_as_id == 0
+    assert o.transmit_as is Object.get(0)
