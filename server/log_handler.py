069e1203604e888494d744e1252ac239973cd2a5
Chris Norman <chris.norman2@googlemail.com>
2018-04-26 21:44:41 +0100

    Don't use contextmanager.2

--- server/log_handler.py
+++ server/log_handler.py
@@ -1,7 +1,7 @@
 """Provides the LogHandler class."""
 
 from logging import Handler
-from .db import session, CommunicationChannel, ServerOptions
+from .db import Session as s, CommunicationChannel, ServerOptions
 
 
 class LogHandler(Handler):
@@ -10,17 +10,16 @@ class LogHandler(Handler):
     def emit(self, record):
         """Send the record in game."""
         name = record.levelname
-        with session() as s:
-            channel = CommunicationChannel.query(name=name, admin=True).first()
-            if channel is None:
-                channel = CommunicationChannel(
-                    description=f'Log messages from the {name} category.',
-                    name=name, admin=True
-                )
-                s.add(channel)
-                s.commit()
-            sender = ServerOptions.get().transmit_as
-            assert sender is not None
-            channel.transmit(
-                sender, f'{record.name}: {record.getMessage()}', strict=False
+        channel = CommunicationChannel.query(name=name, admin=True).first()
+        if channel is None:
+            channel = CommunicationChannel(
+                description=f'Log messages from the {name} category.',
+                name=name, admin=True
             )
+            s.add(channel)
+            s.commit()
+        sender = ServerOptions.get().transmit_as
+        assert sender is not None
+        channel.transmit(
+            sender, f'{record.name}: {record.getMessage()}', strict=False
+        )
